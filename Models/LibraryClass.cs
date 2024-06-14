211957b215ae1ccac12db4ba9b07187746a225eb
Aston Ponnusamy <astonelijah0930@gmail.com>
2024-06-15 00:10:17 +0200

    something#######

--- Models/LibraryClass.cs
+++ Models/LibraryClass.cs
@@ -2,7 +2,27 @@
 {
     public class LibraryClass
     {
+        public class People
+        {
+            private string _firstName { get; set; }
+            private string _lastName { get; set; }
+            private int _personId { get; set; }
+            private string _contactNumber { get; set; }
+            private string _Address { get; set; }
+            private string _Gender { get; set; }
+            private string _Email { get; set; }
+            public People(string firstName, string lastName, string contactNumber, string Address, string Gender, string Email)
+            {
+                _firstName = firstName;
+                _lastName = lastName;
+                _contactNumber = contactNumber;
+                _Address = Address;
+                _Gender = Gender;
+                _Email = Email;
 
+            }
+
+        }
         public class Book
         {
             private string p_author;

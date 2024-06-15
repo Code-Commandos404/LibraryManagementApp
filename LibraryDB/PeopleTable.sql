51581ca0335399b5c05cb49b4a6ce7589c2ac895
Kian <75130117+Kian1010@users.noreply.github.com>
2024-06-15 21:45:03 +0200

    Adding in SQLLite DB and tables

---
+++ LibraryDB/PeopleTable.sql
@@ -0,0 +1,11 @@
+﻿-- Script Date: 2024/06/15 21:03  - ErikEJ.SqlCeScripting version 3.5.2.95
+CREATE TABLE [People] (
+  [PersonID] NUMERIC NOT NULL
+, [FirstName] TEXT NOT NULL
+, [LastName] TEXT NOT NULL
+, [ContactNumber] TEXT NOT NULL
+, [Address] TEXT NOT NULL
+, [Gender] TEXT NOT NULL
+, [Email] TEXT NOT NULL
+, CONSTRAINT [PK_People] PRIMARY KEY ([PersonID])
+);

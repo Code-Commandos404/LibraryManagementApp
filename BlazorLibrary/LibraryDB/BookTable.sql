6a200741cd1064e8a8284792ebb0c5bdca9680a0
Kian <75130117+Kian1010@users.noreply.github.com>
2024-06-17 20:38:07 +0200

    testing book layout

---
+++ BlazorLibrary/LibraryDB/BookTable.sql
@@ -0,0 +1,10 @@
+﻿-- Script Date: 2024/06/15 21:20  - ErikEJ.SqlCeScripting version 3.5.2.95
+CREATE TABLE [Book] (
+  [BookID] INTEGER NOT NULL
+, [Author] TEXT NOT NULL
+, [PublishedDate] TEXT NOT NULL
+, [Title] TEXT NOT NULL
+, [Genre] TEXT NOT NULL
+, [Rating] INTEGER NOT NULL
+, CONSTRAINT [PK_Book] PRIMARY KEY ([BookID])
+);

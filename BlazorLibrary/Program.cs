42395780e04e9695fc7e2f28adad57c5e5841847
Aston Ponnusamy <astonelijah0930@gmail.com>
2024-06-19 22:59:07 +0200

    fixed massive bug

--- BlazorLibrary/Program.cs
+++ BlazorLibrary/Program.cs
@@ -4,6 +4,7 @@ using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddServerSideBlazor().AddCircuitOptions(option => { option.DetailedErrors = true; });
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddTransient<ISqlDb, LibraryClass.SqlDb>(); //Adding a transient service for data which creates an instance every time we ask for it...singleton adds 1 instance for the entire application

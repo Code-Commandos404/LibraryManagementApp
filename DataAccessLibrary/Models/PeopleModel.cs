0b839351e80bad5c95b7171f35793e7ced2fc591
Aston Ponnusamy <astonelijah0930@gmail.com>
2024-06-19 20:51:33 +0200

    changed Person

--- DataAccessLibrary/Models/PeopleModel.cs
+++ DataAccessLibrary/Models/PeopleModel.cs
@@ -1,33 +1,39 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace LibraryManagementApp.Models
 {
     public class PeopleModel
     {
-            [Required]
-            [MaxLength(50, ErrorMessage = "FirstName too long")]
-            [MinLength(1,ErrorMessage ="Firstname too short")]
-            public string FirstName { get; set; }
+		public int PersonID { get; set; };
 
-		    [Required]
-		    [MaxLength(50, ErrorMessage = "Lastname too long")]
-		    [MinLength(1, ErrorMessage = "Lastname too short")]
-		    public string LastName { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "FirstName too long")]
+        [MinLength(1,ErrorMessage ="Firstname too short")]
+        public string FirstName { get; set; }
 
-            [Required]
-		    [MaxLength(12, ErrorMessage = "Contact Number too long")]
-		    public string ContactNumber { get; set; }
+		[Required]
+		[MaxLength(50, ErrorMessage = "Lastname too long")]
+		[MinLength(1, ErrorMessage = "Lastname too short")]
+		public string LastName { get; set; }
 
-		    [Required]
-		    [MaxLength(100, ErrorMessage = "Address too long")]
-		    public string Address { get; set; }
+        [Required]
+		[MaxLength(12, ErrorMessage = "Contact Number too long")]
+		public string ContactNumber { get; set; }
 
-            [Required]
-            public string Gender { get; set; }
+		[Required]
+		[MaxLength(100, ErrorMessage = "Address too long")]
+		public string Address { get; set; }
 
-			[Required]
-			[EmailAddress]
-			public string Email { get; set; }
+        [Required]
+        public string Gender { get; set; }
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[Required]
+		public string Password { get; set; }
 
     }
 }
